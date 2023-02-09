43b9985127fdf58f80b1ca9d34c7d5295219670a
brunovonkoln <brunovonkoln@gmail.com>
2023-02-09 18:07:08 -0300

    ..

--- main.js
+++ main.js
@@ -2,10 +2,28 @@ function criaCalculadora()
 {
   return
   {
+    display: document.querySelector('#result span'),
+
     inicia()
     {
-      alert('Oi, iniciei')
+      this.cliqueBotoes()
     }
+
+    cliqueBotoes()
+    {
+      // this -> calculadora
+      document.addEventListener('click', function(e)
+      {
+       const el = e.target
+       
+       if(el.classList.contains('btn-num'))
+       {
+        this.btnParaDisplay()
+       }
+      })
+    }
+
+    btnParaDisplay()
   }
 }
 
